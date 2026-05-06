6ab5b98df0f169c087cb99ed6e3f114830e7adbd
antunesdediogo-design <antunes.de.diogo@gmail.com>
2026-05-06 11:46:03 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/Google_AI_S_PsiqueSim-W-Voice-/'
       server: {
         port: 3000,
         host: '0.0.0.0',
